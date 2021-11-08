fd15e6539f3842e6b843e8de68fe265e2c346a5b
DavidRamirez09 <ramirez.barrera.david.20@gmail.com>
2021-11-07 23:09:58 -0600

    Calculadora

---
+++ app/src/main/java/com/david/ramirez/barrera/logica/Calculadora.java
@@ -0,0 +1,19 @@
+package com.david.ramirez.barrera.logica;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable {
+
+    public void suma(){
+
+    }
+    public void resta(){
+
+    }
+    public void multiplicacion(){
+
+    }
+    public void division(){
+
+    }
+}
